fe5dc3a79dcf3aea52e049773e0415518bd02964
Mahmoud Tareq <mahmoudtarek.1335@gmail.com>
2023-07-13 15:51:09 +0300

    Add project files.

---
+++ STG1_D5/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace STG1_D5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter student information");
+            Student2 student = new Student2();
+           
+           
+            Console.WriteLine(student.PrintAsString());
+
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Enter number of students");
+            int size = int.Parse(Console.ReadLine());
+            Student2[] students = new Student2[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Enter student{i + 1} information");
+                students[i] = new Student2();
+               
+
+            }
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Student{i + 1}");
+                Console.WriteLine(students[i].PrintAsString());
+            }
+
+        }
+        
+    }
+}
---
+++ STG1_D5/Student.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STG1_D5
+{
+    internal class Student2
+    {
+        private string ssn;
+        private string name;
+        private int age;
+        private string address;
+        static int counter;
+        public Student2()
+        {
+            read();
+            counter++;
+            if (counter%10 == 0) {
+                Console.WriteLine("There is another 10 students have been submitted");
+            }
+        }
+        #region setters
+        //public void setSsn(string _ssn)
+        //{
+        //    ssn = _ssn;
+        //}
+        //public void setName(string _name)
+        //{
+        //    name = _name;
+        //}
+        //public void setAge(int _age)
+        //{
+        //    age = _age;
+        //}
+        //public void setAddress(string _address)
+        //{
+        //    address = _address;
+        //}
+        #endregion
+        #region getters
+        //public string getSsn()
+        //{
+        //    return ssn;
+        //}
+        //public string getName()
+        //{
+        //    return name;
+        //}
+        //public int getAge()
+        //{
+        //    return age;
+        //}
+        //public string getAddress()
+        //{
+        //    return address;
+        //}
+
+        #endregion
+        #region property
+        public string Ssn
+        {
+            get { return ssn; }
+            set { ssn = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        #endregion
+        public string PrintAsString()
+        {
+            return $"Employee ssn is {this.ssn},\n" +
+                $"Employee  name is {this.name}\n" +
+                $"Employee age is {this.age}\n" +
+                $"Employee address is {this.address}\n";
+        }
+        public void read()
+        {
+            Console.WriteLine("Enter Student national id");
+            Ssn = (Console.ReadLine());
+            Console.WriteLine("Enter Student name");
+            Name = (Console.ReadLine());
+            Console.WriteLine("Enter Student age");
+            Age = (int.Parse(Console.ReadLine()));
+            Console.WriteLine("Enter Student address");
+            Address = (Console.ReadLine());
+        }
+    }
+}
